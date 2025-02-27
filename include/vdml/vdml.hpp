7af7833e73371dc5af19decf4bd1144ee4959adf
Exortions <75327059+Exortions@users.noreply.github.com>
2025-02-26 22:03:26 -0800

    ♻️ Refactor vdml namespace under zest namespace

--- include/vdml/vdml.hpp
+++ include/vdml/vdml.hpp
@@ -4,11 +4,13 @@
 #include <array>
 #include <mutex>
 
+namespace zest {
 namespace vdml {
 extern std::array<std::mutex, 21> port_mutexes;
 
 std::mutex& smart_port_mutex(int port);
 
 }  // namespace vdml
+}  // namespace zest
 
 #endif  // VDML_HPP
