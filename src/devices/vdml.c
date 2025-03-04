2315e7a1f2de3654e34a234a4b5ca2e9aa9d4c77
Liam Teale <liamteale6@gmail.com>
2025-03-03 22:32:42 -0800

    fix vdml.c lints

--- src/devices/vdml.c
+++ src/devices/vdml.c
@@ -16,11 +16,9 @@
 #include "vdml/vdml.h"
 
 #include "kapi.h"
-#include "v5_api.h"
 #include "vdml/registry.h"
 
 #include <errno.h>
-#include <stdio.h>
 
 /**
  * Bitmap to indicate if a port has had an error printed or not.
