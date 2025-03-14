d8880de5fe90467a9410624bb32930951c426c0c
ion098 <146852218+ion098@users.noreply.github.com>
2025-03-14 17:26:02 +0000

    refactor: :label: Use more idiomatic c++ types in vfs, add FileEntry

--- include/system/dev/vfs.hpp
+++ include/system/dev/vfs.hpp
@@ -1,17 +1,25 @@
 #pragma once
 
+#include <any>
 #include <compare>
 #include <cstddef>
 #include <cstdint>
+#include <cstdio>
+#include <memory>
+#include <span>
 #include <sys/stat.h>
 #include <unistd.h>
 
-namespace zest {
+namespace zest::fs {
 
 class FileDescriptor {
   public:
-    FileDescriptor(int fd)
+    constexpr FileDescriptor(int fd)
         : m_fd(fd) {}
+
+    explicit operator int() const {
+        return m_fd;
+    }
 
     std::strong_ordering operator<=>(const FileDescriptor&) const = default;
 
@@ -19,16 +27,31 @@ class FileDescriptor {
     int m_fd;
 };
 
-class FileDriver {
+inline constexpr FileDescriptor FD_STDIN{0};
+inline constexpr FileDescriptor FD_STDOUT{1};
+inline constexpr FileDescriptor FD_STDERR{2};
+
+class FileDriver : public std::enable_shared_from_this<FileDriver> {
   public:
-    virtual FileDescriptor open(const char* path, int flags, int mode) = 0;
-    virtual ssize_t read(void* const, uint8_t*, const size_t) = 0;
-    virtual int write(void* const, uint8_t*, const size_t) = 0;
-    virtual int close(void* const) = 0;
-    virtual int fstat(void* const, struct stat*) = 0;
-    virtual int isatty(void* const) = 0;
-    virtual off_t lseek(void* const, off_t, int) = 0;
-    virtual int ctl(void* const, const uint32_t, void* const) = 0;
     virtual ~FileDriver() = default;
+
+  protected:
+    virtual void init() = 0;
+    virtual FileDescriptor open(const char* path, int flags, int mode) = 0;
+    virtual ssize_t read(std::any, std::span<std::byte>) = 0;
+    virtual int write(std::any, std::span<std::byte>) = 0;
+    virtual int close(std::any) = 0;
+    virtual int fstat(std::any, struct stat*) = 0;
+    virtual int isatty(std::any) = 0;
+    virtual off_t lseek(std::any, off_t, int) = 0;
+    virtual int ctl(std::any, const uint32_t, void* const) = 0;
+    FileDescriptor add_vfs_entry(std::any data);
+    int32_t update_vfs_entry(FileDescriptor fd, std::any data);
 };
-} // namespace zest
+
+struct FileEntry {
+  std::shared_ptr<FileDriver> driver;
+  std::any data;
+};
+
+} // namespace zest::fs
