9a56d9f764823b7736fc549260829ab74ec5d903
ion098 <146852218+ion098@users.noreply.github.com>
2025-04-10 18:17:21 +0000

    feat: :sparkles: Implement all UsdDriver methods and add more error handling

--- include/system/vfs/file_driver.hpp
+++ include/system/vfs/file_driver.hpp
@@ -19,12 +19,12 @@ class FileDriver : public std::enable_shared_from_this<FileDriver> {
     virtual void init() = 0;
     virtual std::expected<FileDescriptor, std::error_condition>
     open(const char* path, int flags, int mode) = 0;
-    virtual ssize_t read(std::any, std::span<std::byte>) = 0;
-    virtual int write(std::any, std::span<std::byte>) = 0;
-    virtual int close(std::any) = 0;
-    virtual int fstat(std::any, struct stat*) = 0;
-    virtual int isatty(std::any) = 0;
-    virtual off_t lseek(std::any, off_t, int) = 0;
+    virtual std::expected<ssize_t, std::error_condition> read(std::any, std::span<std::byte>) = 0;
+    virtual std::expected<int, std::error_condition> write(std::any, std::span<std::byte>) = 0;
+    virtual std::expected<void, std::error_condition> close(std::any) = 0;
+    virtual std::expected<void, std::error_condition> fstat(std::any, struct stat*) = 0;
+    virtual bool isatty(std::any) = 0;
+    virtual std::expected<off_t, std::error_condition> lseek(std::any, off_t, int) = 0;
     virtual int ctl(std::any, const uint32_t, void* const) = 0;
     FileDescriptor add_vfs_entry(std::any data);
     int32_t update_vfs_entry(FileDescriptor fd, std::any data);
--- include/system/vfs/usd_driver.hpp
+++ include/system/vfs/usd_driver.hpp
@@ -2,6 +2,9 @@
 
 #include "pros/rtos.hpp"
 #include "system/vfs/file_driver.hpp"
+#include "v5_apitypes_patched.h"
+
+#include <mutex>
 
 namespace zest::fs {
 class UsdDriver : public FileDriver {
@@ -10,16 +13,27 @@ class UsdDriver : public FileDriver {
 
     std::expected<FileDescriptor, std::error_condition>
     open(const char* path, int flags, int mode) override;
-    ssize_t read(std::any, std::span<std::byte>) override;
-    int write(std::any, std::span<std::byte>) override;
-    int close(std::any) override;
-    int fstat(std::any, struct stat*) override;
-    int isatty(std::any) override;
-    off_t lseek(std::any, off_t, int) override;
+    std::expected<ssize_t, std::error_condition> read(std::any, std::span<std::byte>) override;
+    std::expected<int, std::error_condition> write(std::any, std::span<std::byte>) override;
+    std::expected<void, std::error_condition> close(std::any) override;
+    std::expected<void, std::error_condition> fstat(std::any, struct stat*) override;
+    bool isatty(std::any) override;
+    std::expected<off_t, std::error_condition> lseek(std::any, off_t, int) override;
     int ctl(std::any, const uint32_t, void* const) override;
     ~UsdDriver() override = default;
 
   private:
+    struct File {
+        FIL* fd;
+        bool is_writable;
+    };
+
+    template<typename F, typename... Args>
+    auto inline with_lock(F&& fun, Args... args) {
+        std::lock_guard lock{this->mut};
+        return fun(args...);
+    }
+
     pros::RecursiveMutex mut{};
 };
 
