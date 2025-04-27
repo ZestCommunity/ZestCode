ac4609ef9e1ed46232b4eb1f5b68644a50b318ed
Liam Teale <liamteale6@gmail.com>
2025-04-27 14:13:04 -0700

    use error types in Result class

--- include/common/result.hpp
+++ include/common/result.hpp
@@ -1,118 +1,146 @@
 #pragma once
 
-#include <format>
-#include <iostream>
+#include <concepts>
+#include <limits>
 #include <optional>
 #include <stacktrace>
 #include <type_traits>
-
-// library developers and advanced users may appreciate compiler checks to ensure they are actually
-// handling all errors.
-// By defining EXPLICIT_RESULTS, the Result conversion operators will be marked as explicit.
-// This does not break ABI, so libraries/projects dependencies can define EXPLICIT_RESULTS or not
-// and still be compatible.
-#ifndef EXPLICIT_RESULTS
-    #define RESULT_EXPLICIT_QUALIFIER
-#else
-    #define RESULT_EXPLICIT_QUALIFIER explicit
-#endif
+#include <variant>
 
 namespace zest {
 
-template<typename T>
-    requires std::is_scoped_enum_v<T>
-struct ResultError {
-    std::string message;
+// custom error types inherit from the ResultError class, which enforces that custom error types
+// have some shared functionality.
+class ResultError {
+  public:
+    // since this constructor has no arguments, it'll be called implicitly by the constructor of any
+    // child class.
+    ResultError()
+        : stacktrace(std::stacktrace::current()) {}
+
     std::stacktrace stacktrace;
-    T type;
 };
 
-/**
- * @brief Result class, used for error handling.
- *
- * The Result class can contain a value and optionally an error. Unlike std::expected, the value is
- * ALWAYS valid. This means that a failure to handle an error by the user will not crash the thread.
- *
- * Instances of this class can be implicitly converted to the "normal" type, so UX is not
- * complicated at all.
- *
- * @tparam T the "normal" value.
- * @tparam E the error type, has to be an enum class.
- *
- * @b Example
- * @code
- * enum class ExampleError {
- *     ExampleA,
- *     ExampleB,
- * };
- *
- * zest::Result<double, ExampleError> get_double() {
- *     // for the purposes of this example, this function will always return an error
- *     return {INFINITY, ExampleError::ExampleA, "I can be formatted{}", "!!!"};
- * }
- *
- * void initialize() {
- * }
- *
- * @endcode
- */
-template<typename T, typename E>
-    requires std::is_scoped_enum_v<E>
+// concept used to enforce that error types inherit from the ResultError class
+template<typename T>
+concept CustomError = std::derived_from<T, ResultError>;
+
+// Some primitive types such as float, double, etc may have a standardized "sentinel" value.
+// For example, a function that returns a float may return INFINITY if an error occurs.
+// Usually the maximum value of the type is returned if there is an error.
+// This trait helps simplify DX.
+// This trait can also be specialized for custom types (e.g unitized types)
+template<typename T>
+class HasSentinel;
+
+// concept which can be used to determine if a type has a sentinel value
+template<typename T>
+concept Sentinel = requires(const T& val) { HasSentinel<T>::value; };
+
+// templated variable which can be used to simplify usage of HasSentinel
+template<Sentinel T>
+constexpr T sentinel_v = HasSentinel<T>::value;
+
+// partial specialization for HasSentinel.
+// any integral type (e.g double, int, uint8, etc) has a sentinel value equal to its maximum value
+template<std::integral T>
+class HasSentinel<T> {
+    static constexpr T value = std::numeric_limits<T>::max();
+};
+
+// Result class.
+// An alternative to std::expected, where an expected value can be contained alongside an unexpected
+// error value.
+// This means that Result will always contain an expected value.
+// Therefore, if the user does not check if a function returned an error, an exception will NOT be
+// thrown, and therefore the thread won't crash.
+template<typename T, CustomError... Errs>
 class Result {
   public:
+    // Construct a Result with a value and no error value.
+    // Constraint: the value member variable must be able to be constructed with the value
+    // parameter.
     template<typename U>
         requires std::constructible_from<T, U>
-    Result(U&& val)
-        : val(std::forward<T>(val)) {}
+    constexpr Result(U&& value)
+        : value(std::forward<U>(value)) {}
 
-    template<typename U, typename F>
-        requires std::constructible_from<T, U> && std::constructible_from<ResultError<E>, F>
-    Result(U&& val, F&& error)
-        : val(std::forward<T>(val)),
-          error(std::forward<ResultError>(error)) {}
-
-    template<typename U, typename... Args>
+    // Construct a Result with a value and an error value.
+    // Constraint: the value member variable must be able to be constructed with the value
+    // parameter.
+    // Constraint: the error parameter must be of a type that may be contained in the error member
+    // variable.
+    template<typename U, CustomError E>
         requires std::constructible_from<T, U>
-    Result(U&& val, E type, std::format_string<Args...> fmt, Args&&... args)
-        : val(std::forward<T>(val)),
-          error({type, std::format(fmt, std::forward<Args>(args)...), std::stacktrace::current()}) {
+                     && (std::is_same_v<Errs, std::remove_cvref<E>> || ...)
+    constexpr Result(U&& value, E&& error)
+        : value(std::forward<U>(value)),
+          error(std::forward<E>(error)) {}
+
+    // Construct a Result with an error, initializing the normal value to its sentinel value.
+    // Constraint: the type of the normal value must have a specialized sentinel value.
+    // Constraint: the error parameter must be of a type that may be contained in the error member
+    // variable.
+    template<CustomError U>
+        requires Sentinel<T> && (std::is_same_v<Errs, std::remove_cvref<U>> || ...)
+    constexpr Result(U&& error)
+        : value(sentinel_v<T>),
+          error(std::forward<U>(error)) {}
+
+    // Check whether the result contains an error of the given type
+    // Constraint: the custom error type to check for must be able to be contained in the error
+    // member variable.
+    template<CustomError U>
+        requires(std::is_same_v<Errs, std::remove_cvref<U>> || ...)
+    constexpr U has() const& {
+        if (!error.has_value()) {
+            return false;
+        } else {
+            return std::holds_alternative<U>(error);
+        }
+    };
+
+    // return an optional wrapping the given error type.
+    // Constraint: the custom error type to get must be able to be contained in the error
+    // member variable.
+    template<CustomError U>
+        requires(std::is_same_v<Errs, std::remove_cvref<U>> || ...)
+    constexpr std::optional<U> get() const& {
+        if (this->has<U>()) {
+            return std::get<U>(error.value());
+        } else {
+            return std::nullopt;
+        }
     }
 
-    RESULT_EXPLICIT_QUALIFIER operator T() const& {
-        return val;
+    // return an optional wrapping the given error type.
+    // Constraint: the custom error type to get must be able to be contained in the error
+    // member variable.
+    template<CustomError U>
+        requires(std::is_same_v<Errs, std::remove_cvref<U>> || ...)
+    constexpr std::optional<U> get() && {
+        if (this->has<U>()) {
+            return std::get<U>(std::move(error.value()));
+        } else {
+            return std::nullopt;
+        }
     }
 
-    RESULT_EXPLICIT_QUALIFIER operator T() && {
-        return std::move(val);
+    // implicit conversion operator, on an l-value
+    constexpr operator T() const& {
+        return value;
+    };
+
+    // implicit conversion operator, on an r-value
+    constexpr operator T() && {
+        return std::move(value);
     }
 
-    T val;
-    std::optional<ResultError<E>> error;
+    // the optional error value.
+    // a variant that could contain any of the specified error types.
+    std::optional<std::variant<Errs...>> error;
+    // the normal value
+    T value;
 };
 
 } // namespace zest
-
-template<typename T>
-struct std::formatter<zest::ResultError<T>> {
-    constexpr auto parse(std::format_parse_context& ctx) const {
-        return ctx.begin();
-    }
-
-    auto format(const zest::ResultError<T>& error, std::format_context& ctx) const {
-        return std::format_to(
-            ctx.out(),
-            "{}\n"
-            "<== BEGIN STACKTRACE ==>\n"
-            "{}\n"
-            "<== END STACKTRACE ==>",
-            error.message,
-            error.stacktrace
-        );
-    }
-};
-
-template<typename T>
-std::ostream& operator<<(std::ostream& os, const zest::ResultError<T>& error) {
-    os << std::format("{}", error);
-    return os;
-}
