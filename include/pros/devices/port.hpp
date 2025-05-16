e9db49970b3d89a6bf000529e0b6355f14b6dc33
Liam Teale <liamteale6@gmail.com>
2025-05-16 13:20:53 -0700

    port bugfixes

--- include/pros/devices/port.hpp
+++ include/pros/devices/port.hpp
@@ -1,38 +1,34 @@
 #pragma once
 
-#include "pros/rtos.hpp"
-
-#include <cctype>
+#include <cstdint>
 
 namespace zest {
-
 class SmartPort {
   public:
-    static constexpr SmartPort from_number(uint8_t number) {
-        return SmartPort(number - 1);
+    static constexpr SmartPort from_number(uint8_t port_number) {
+        return SmartPort(port_number - 1);
     }
 
-    static constexpr SmartPort from_index(uint8_t index) {
-        return SmartPort(index);
+    static constexpr SmartPort from_index(uint8_t port_index) {
+        return SmartPort(port_index);
     }
 
-    constexpr uint8_t as_number() {
-        return m_index - 1;
+    constexpr uint8_t as_number() const {
+        return m_index + 1;
     }
 
-    constexpr uint8_t as_index() {
+    constexpr uint8_t as_index() const {
         return m_index;
     }
 
   private:
-    constexpr SmartPort(uint8_t index)
-        : m_index(index) {}
+    constexpr SmartPort(int port_index)
+        : m_index(port_index) {}
 
     uint8_t m_index;
 };
 
 namespace ports {
-
 constexpr auto PORT_1 = SmartPort::from_number(1);
 constexpr auto PORT_2 = SmartPort::from_number(2);
 constexpr auto PORT_3 = SmartPort::from_number(3);
@@ -54,50 +50,51 @@ constexpr auto PORT_18 = SmartPort::from_number(18);
 constexpr auto PORT_19 = SmartPort::from_number(19);
 constexpr auto PORT_20 = SmartPort::from_number(20);
 constexpr auto PORT_21 = SmartPort::from_number(21);
-
 } // namespace ports
 
 class AdiPort {
   public:
-    static constexpr AdiPort from_char(char port) {
-        return AdiPort(port);
+    static constexpr AdiPort from_letter(char port) {
+        // if the index is provided as a character
+        if (port >= '0' && port <= '9') {
+            return AdiPort(port - '0');
+        }
+        // convert to uppercase if needed
+        if (port >= 'a' && port <= 'z') {
+            port -= ('a' - 'A');
+        }
+        // 'A' has index 0, 'B' has index 1, etc
+        return AdiPort(port - 'A');
     }
 
-    constexpr char as_char() {
-        return m_port;
+    static constexpr AdiPort from_index(uint8_t index) {
+        return AdiPort(index);
     }
 
-    constexpr uint8_t as_index() {
-        return static_cast<uint8_t>(m_port) - 65;
+    constexpr char as_letter() const {
+        return m_index;
+    }
+
+    constexpr uint8_t as_index() const {
+        return m_index;
     }
 
   private:
-    constexpr AdiPort(char port) {
-        // convert lowercase to uppercase if needed
-        if (port >= 97) {
-            port -= 32;
-        }
-        m_port = port;
-    }
+    uint8_t m_index;
 
-    char m_port;
+    constexpr AdiPort(uint8_t index)
+        : m_index(index) {}
 };
 
 namespace ports {
-
-constexpr auto PORT_A = AdiPort::from_char('A');
-constexpr auto PORT_B = AdiPort::from_char('B');
-constexpr auto PORT_C = AdiPort::from_char('C');
-constexpr auto PORT_D = AdiPort::from_char('D');
-constexpr auto PORT_E = AdiPort::from_char('E');
-constexpr auto PORT_F = AdiPort::from_char('F');
-constexpr auto PORT_G = AdiPort::from_char('G');
-constexpr auto PORT_H = AdiPort::from_char('H');
+constexpr auto PORT_A = AdiPort::from_letter('A');
+constexpr auto PORT_B = AdiPort::from_letter('B');
+constexpr auto PORT_C = AdiPort::from_letter('C');
+constexpr auto PORT_D = AdiPort::from_letter('D');
+constexpr auto PORT_E = AdiPort::from_letter('E');
+constexpr auto PORT_F = AdiPort::from_letter('F');
+constexpr auto PORT_G = AdiPort::from_letter('G');
+constexpr auto PORT_H = AdiPort::from_letter('H');
 
 } // namespace ports
-
-template<typename T>
-pros::RecursiveMutex& get_port_mutex(T&&) {
-    throw("can't get mutex for non-port object!");
-}
 } // namespace zest
