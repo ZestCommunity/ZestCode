451f06a4b8b35cf24765d3e3834bbe31b5ed1cd8
Liam Teale <liamteale6@gmail.com>
2025-06-17 22:31:37 -0700

    further simplify traits, improve trait documentation

--- include/common/result.hpp
+++ include/common/result.hpp
@@ -12,6 +12,26 @@
 
 namespace zest {
 
+/**
+ * @brief lambda argument type helper
+ *
+ * Yoinked from c++ stories https://www.cppstories.com/2019/02/2lines3featuresoverload.html/
+ *
+ * @b Example
+ * @code
+ * // output: hello
+ * std::variant<int, float, std::string> int_float_string("Hello");
+ * std::visit(overload {
+ *         [](const int& i) { std::println("int: {}", i); },
+ *         [](const float& f) { std::println("float: {}", f); },
+ *         [](const std::string& s) { std::println("string: {}", s); }
+ *     },
+ *     int_float_string
+ * );
+ * @endcode
+ *
+ * @tparam Ts
+ */
 template<class... Ts>
 struct overload : Ts... {
     using Ts::operator()...;
@@ -36,6 +56,7 @@ class ResultError {
         std::chrono::time_point<std::chrono::system_clock> time;
     };
 
+    // the runtime data is only contained if ResultError was constructed at runtime
     std::optional<RuntimeData> runtime_data;
 
   protected:
@@ -65,7 +86,11 @@ namespace traits {
 template<typename T, typename... Ts>
 concept InPack = (std::is_same_v<std::remove_cvref_t<T>, Ts> || ...);
 
-// A simple type pack wrapper
+/**
+ * @brief a simple typename pack wrapper
+ *
+ * @tparam Ts the typename pack to wrap
+ */
 template<typename... Ts>
 struct type_pack {};
 
@@ -96,35 +121,30 @@ struct contains_all<type_pack<As...>, type_pack<Bs...>> {
  * @tparam PackB
  */
 template<typename PackA, typename PackB>
-inline constexpr bool contains_all_v = contains_all<PackA, PackB>::value;
+concept ContainsAll = contains_all<PackA, PackB>::value;
 
 /**
- * @brief Check if PackA contains all the types in PackB
- *
- * @tparam PackA
- * @tparam PackB
- */
-template<typename PackA, typename PackB>
-concept ContainsAll = contains_all_v<PackA, PackB>;
-
-/**
- * @brief IsResultError concept. Enforces that the given type is derived from the ResultError class
+ * @brief Check if a given type is derived from ResultError
  */
 template<typename T>
-concept IsResultError = std::is_base_of_v<ResultError, T>;
+concept IsResultError = std::is_base_of_v<ResultError, std::remove_cvref_t<T>>;
 
 /**
  * @brief Trait to define a "sentinel" value for types indicating an error state.
- * @tparam T Type to provide a sentinel value for.
+ *
  * @note Specialize this template for custom types if needed.
+ *
+ * @tparam T Type to provide a sentinel value for.
  */
 template<typename T>
 class SentinelValue;
 
 /**
  * @brief Partial specialization of SentinelValue for integral and floating-point types.
- * @tparam T Integral or floating-point type.
+ *
  * @details Uses infinity for floating-point types if available; otherwise uses max value.
+ *
+ * @tparam T Integral or floating-point type.
  */
 template<typename T>
     requires(std::integral<T> || std::floating_point<T>)
@@ -157,7 +177,14 @@ inline constexpr T sentinel_v = SentinelValue<T>::value;
 
 } // namespace traits
 
-// forward declarations, necessary to declare traits
+/**
+ * @brief Result class for expected value or error handling (similar to std::expected).
+ *
+ * @note the "normal" value is always valid, but an "error" value may or may not be contained.
+ *
+ * @tparam T Type of the expected value. Must have
+ * @tparam Errs List of possible error types (must inherit from ResultError).
+ */
 template<typename T, traits::IsResultError... Errs>
     requires(traits::HasSentinel<T> || std::same_as<void, T>) && (sizeof...(Errs) > 0)
 class Result;
@@ -165,62 +192,70 @@ class Result;
 namespace traits {
 
 /**
- * @brief Type trait to check if a type is a Result.
+ * @brief Check if a type is a Result
+ *
  * @tparam T Type to check.
  */
 template<typename>
 struct is_result : std::false_type {};
 
-/**
- * @brief Type trait to check if a type is a Result.
- * @tparam T Type to check.
- */
+// is_result specialization. Used to infer Result.
 template<typename T, typename... Errs>
 struct is_result<Result<T, Errs...>> : std::true_type {};
 
 /**
- * @brief Check whether a type is a Result
+ * @brief Check if a type is a Result
+ *
  * @tparam T Type to check
  */
 template<typename T>
-inline constexpr bool is_result_v = is_result<T>::value;
+concept IsResult = is_result<T>::value;
 
 /**
- * @brief Concept to check if a type is a Result
- * @tparam T Type to check
+ * @brief Used to show that a type should be ignored
+ *
  */
-template<typename T>
-concept IsResult = is_result_v<T>;
-
-// special type that is ignored by concepts like all_same
 struct ignored_type {};
 
 /**
  * @brief Check whether all types in a pack are the same
  *
+ * @note any ignored_type will not be considered
+ *
  * @tparam Ts types to check
  */
 template<typename... Ts>
-struct all_same : std::true_type {};
+inline constexpr bool all_same_v = true;
+
+// this specialization uses recursion to check all the types.
+// This is the only known way to do this in C++23 (because of the extra logic needed for
+// ignored_type)
+// TODO: use C++26 pack indexing once it's supported
+template<typename T, typename... Ts>
+inline constexpr bool all_same_v<T, Ts...> =
+    ((std::same_as<T, Ts> || std::same_as<ignored_type, Ts>) && ...);
 
 /**
  * @brief Check whether all types in a pack are the same
  *
- * @tparam T first type
- * @tparam Ts the rest of the types
+ * @note any ignored_type will not be considered
+ *
+ * @tparam Ts types to check
  */
-template<typename T, typename... Ts>
-struct all_same<T, Ts...> :
-    std::bool_constant<((std::is_same_v<T, Ts> || std::is_same_v<ignored_type, Ts>) && ...)> {};
-
 template<typename... Ts>
-concept AllSame = all_same<Ts...>::value;
+concept AllSame = all_same_v<Ts...>;
 
-// Primary template: no types, no result (causes substitution failure if used)
+/**
+ * @brief Find the first type in a typename pack that satisfies the given condition
+ *
+ * @tparam Condition the condition to check
+ * @tparam Ts the types to apply to the condition
+ */
 template<typename Condition, typename... Ts>
 struct first_type_that_satisfies {};
 
-// Specialization: at least one type in the pack
+// This specialization uses recursion to find the first type.
+// TODO: use C++26 pack indexing once it's supported
 template<typename Condition, typename T, typename... Ts>
 struct first_type_that_satisfies<Condition, T, Ts...> :
     std::conditional<
@@ -229,62 +264,121 @@ struct first_type_that_satisfies<Condition, T, Ts...> :
         first_type_that_satisfies<Condition, Ts...> // Otherwise, check rest
         > {};
 
-// Helper alias to access the result type
+/**
+ * @brief Find the first type in a typename pack that satisfies the given condition
+ *
+ * @tparam Condition the condition to check
+ * @tparam Ts the types to apply to the condition
+ */
 template<typename Condition, typename... Ts>
 using first_type_that_satisfies_t = typename first_type_that_satisfies<Condition, Ts...>::type;
 
+/**
+ * @brief get the return type of a callable invoked with the given arguments
+ *
+ * @note if the callable is not invocable, the type is ignored_type
+ *
+ * @tparam F the callable to invoke
+ * @tparam Args callable argument
+ */
 template<typename F, typename... Args>
-struct invoke_result {
-    using type = ignored_type;
-};
+struct invoke_result : std::type_identity<ignored_type> {};
 
+// specialization, used when the callable is invocable
 template<typename F, typename... Args>
     requires std::invocable<F, Args...>
-struct invoke_result<F, Args...> : std::invoke_result<F, Args...> {};
+struct invoke_result<F, Args...> : std::type_identity<std::invoke_result_t<F, Args...>> {};
 
+/**
+ * @brief get the return type of a callable invoked with the given arguments
+ *
+ * @note if the callable is not invocable, the type is ignored_type
+ *
+ * @tparam F the callable to invoke
+ * @tparam Args callable argument
+ */
 template<typename F, typename... Args>
 using invoke_result_t = invoke_result<F, Args...>::type;
 
-// and_then return type helper
-template<typename Self, typename F>
-using and_then_return_t = std::invoke_result_t<F, decltype((std::declval<Self>().value))>;
+/**
+ * @brief get the return type of Result::and_then given the Result type and callable type
+ *
+ * @tparam R the Result
+ * @tparam F the callable
+ */
+template<typename R, std::invocable F>
+using and_then_return_t = std::invoke_result_t<F, decltype((std::declval<R>().value))>;
 
-// or_else return type helper
-template<typename Self, typename F, typename E>
-using or_else_return_t =
-    traits::invoke_result_t<F, decltype((std::get<E>(std::declval<Self>().error)))>;
+/**
+ * @brief get the return type of the callable passed to Result::or_else
+ *
+ * @note if the callable is not invocable, the type is ignored_type
+ *
+ * @tparam R the Result
+ * @tparam F the callable
+ * @tparam E the callable argument
+ */
+template<typename R, typename F, typename E>
+using or_else_return_t = invoke_result_t<F, decltype((std::get<E>(std::declval<R>().error)))>;
 
-// or_else invocable callable helper
-template<typename Self, typename F, typename E>
-concept Invocable =
-    std::is_invocable<F, decltype((std::get<E>(std::declval<Self>().error)))>::value;
+/**
+ * @brief check whether a callable is invocable given the Result and argument type
+ *
+ * @tparam R the Result
+ * @tparam F the callable
+ * @tparam E the callable argument
+ */
+template<typename R, typename F, typename E>
+concept Invocable = std::invocable<F, decltype((std::get<E>(std::declval<R>().error)))>;
 
-template<typename F, typename Self>
+/**
+ * @brief check whether a callable is invocable given the Result and argument type
+ *
+ * @note the value is wrapped in a struct in order to separate the template arguments. This is done
+ * so this can be used as the condition when using first_type_that_satisfies_t
+ *
+ * @tparam F the callable
+ * @tparam R the Result
+ */
+template<typename F, typename R>
 struct invocable_indirect_v {
+    /**
+     * @brief whether the callable is invocable
+     *
+     * @tparam U the callable argument
+     */
     template<typename U>
-    static constexpr bool value = Invocable<Self, F, U>;
+    static constexpr bool value = Invocable<R, F, U>;
 };
 
-template<typename F, typename Self, typename... Es>
-concept any_invocable = (traits::Invocable<Self, F, Es> || ...);
-
+/**
+ * @brief check whether a type is a Result or void.
+ *
+ * @note helper for Result::or_else
+ * @note concept is satisfied with ignored_type as well
+ *
+ * @tparam T the type to check
+ */
 template<typename T>
 concept ResultOrVoid =
     traits::IsResult<std::remove_cvref_t<T>> || std::same_as<void, std::remove_cvref_t<T>>
-    || std::same_as<ignored_type, T>;
+    || std::same_as<ignored_type, std::remove_cvref_t<T>>;
 
-template<typename R, typename T>
-concept ValueTypeMatch = std::same_as<void, T> || std::same_as<ignored_type, T>
-                         || std::same_as<typename R::value_type, typename T::value_type>;
+/**
+ * @brief check whether the value type of a Result is the other given type
+ *
+ * @note helper for Result::or_else
+ * @note concept if satisfied if T is void or ignored_type
+ *
+ * @tparam R the Result
+ * @tparam T the other type
+ */
+template<typename R, typename S>
+concept ValueTypeMatch = std::same_as<void, S> || std::same_as<ignored_type, S>
+                         || std::same_as<typename S::value_type, typename S::value_type>;
 
 } // namespace traits
 
-/**
- * @brief Result class for expected value or error handling (similar to std::expected).
- * @tparam T Type of the expected value.
- * @tparam Errs List of possible error types (must inherit from ResultError).
- * @note Errors are stored in a variant, and the value is always initialized.
- */
 template<typename T, traits::IsResultError... Errs>
     requires(traits::HasSentinel<T> || std::same_as<void, T>) && (sizeof...(Errs) > 0)
 class Result {
@@ -374,8 +468,8 @@ class Result {
      */
     template<typename Self, typename F>
         requires std::invocable<F, decltype((std::declval<Self>().value))>
-                 && traits::is_result_v<traits::and_then_return_t<Self, F>>
-                 && traits::contains_all_v<
+                 && traits::IsResult<traits::and_then_return_t<Self, F>>
+                 && traits::ContainsAll<
                      typename traits::and_then_return_t<Self, F>::error_types,
                      error_types>
     constexpr auto and_then(this Self&& self, F&& f) {
@@ -413,14 +507,14 @@ class Result {
      * @param f the callable
      */
     template<typename Self, typename F>
-        requires traits::any_invocable<F, Self, Errs...>
-                 // the callable must always return the same type (if it's invocable)
-                 && traits::AllSame<traits::or_else_return_t<Self, F, Errs>...>
-                 // the callable must return a Result or void (if it's invocable)
-                 && (traits::ResultOrVoid<traits::or_else_return_t<Self, F, Errs>> && ...)
-                 // if the callable returns a Result, it must have the same value type as this
-                 // Result instance
-                 && (traits::ValueTypeMatch<Self, traits::or_else_return_t<Self, F, Errs>> && ...)
+        requires(traits::Invocable<Self, F, Errs> || ...)
+                // the callable must always return the same type (if it's invocable)
+                && traits::AllSame<traits::or_else_return_t<Self, F, Errs>...>
+                // the callable must return a Result or void (if it's invocable)
+                && (traits::ResultOrVoid<traits::or_else_return_t<Self, F, Errs>> && ...)
+                // if the callable returns a Result, it must have the same value type as this
+                // Result instance
+                && (traits::ValueTypeMatch<Self, traits::or_else_return_t<Self, F, Errs>> && ...)
     constexpr auto or_else(this Self&& self, F&& f) {
         // the return type is the same as the return type of the callable, unless the callable
         // returns void. In that case, the return type is Self with cv-refs removed
@@ -436,6 +530,7 @@ class Result {
             return ReturnType(std::forward<Self>(self).value);
         }
 
+        // if the callable returns void
         if constexpr (std::same_as<void, CallableReturnType>) {
             return std::visit([&f](auto&& arg) -> ReturnType {
                 // even though this condition is impossible, it's necessary. Otherwise the
@@ -451,6 +546,8 @@ class Result {
                 }
             }, std::forward<Self>(self).error);
         }
+
+        // if the callable returns Result
         return std::visit([&f](auto&& arg) -> ReturnType {
             // even though this condition is impossible, it's necessary. Otherwise the
             // compiler will compile a branch where f is invoked with an unsupported argument
@@ -579,16 +676,15 @@ class Result<void, Errs...> {
      * @return return type of callable
      */
     template<std::invocable F, typename Self>
-        requires traits::is_result_v<std::invoke_result_t<F>>
-                 && traits::
-                     contains_all_v<typename std::invoke_result_t<F>::error_types, error_types>
+        requires traits::IsResult<std::invoke_result_t<F>>
+                 && traits::ContainsAll<typename std::invoke_result_t<F>::error_types, error_types>
     constexpr auto and_then(this Self&& self, F&& f) {
         // if there is an error, return said error immediately
         if (self.has_error()) {
             return std::visit([](auto&& arg) -> std::invoke_result_t<F> {
                 // even though this conditional is impossible, it's necessary to stop the compiler
                 // from thinking that ReturnType could be constructed with std::monostate
-                if constexpr (std::is_same_v<std::monostate, std::remove_cvref_t<decltype(arg)>>) {
+                if constexpr (std::same_as<std::monostate, std::remove_cvref_t<decltype(arg)>>) {
                     throw std::logic_error("This exception is unreachable");
                 } else {
                     return std::forward<decltype(arg)>(arg);
@@ -614,7 +710,7 @@ class Result<void, Errs...> {
      */
     template<typename Self, typename F>
         requires(std::invocable<F, decltype((std::get<Errs>(std::declval<Self>().error)))> && ...)
-                && (traits::is_result_v<or_else_return_t<Self, F, Errs>> && ...)
+                && (traits::IsResult<or_else_return_t<Self, F, Errs>> && ...)
                 && (traits::AllSame<or_else_return_t<Self, F, Errs>...>)
                 && (std::same_as<void, typename or_else_return_t<Self, F, Errs>::value_type> && ...)
     constexpr auto or_else(this Self&& self, F&& f) {
