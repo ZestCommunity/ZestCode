99fb27757c36601c8b2d4af8a84ed8bc4460b068
Liam Teale <liamteale6@gmail.com>
2025-08-26 15:53:16 -0700

    rename has_error overload

--- include/common/result.hpp
+++ include/common/result.hpp
@@ -139,53 +139,9 @@ class Result {
      * @return true error is contained
      * @return false error is not contained
      */
-    constexpr bool has_error(E error) {
+    constexpr bool contains(E error) {
         return this->error == error;
     }
 };
 
 } // namespace zest
-
-/**
- * @brief Compare the equality of 2 Result instances
- *
- * @note this function is defined in order to prevent ambiguous overload resolution.
- *
- * @tparam LhsT "normal" type of left-hand side object
- * @tparam RhsT "normal" type of right-hand side object
- * @tparam LhsE "error" type of left-hand side object
- * @tparam RhsE "error" type of right-hand side object
- *
- * @param lhs left-hand side object
- * @param rhs right-hand side object
- *
- * @return true "normal" values are equal.
- * @return false "normal" values are not equal.
- */
-template<typename LhsT, typename RhsT, typename LhsE, typename RhsE>
-    requires std::equality_comparable_with<LhsT, RhsT>
-constexpr bool
-operator==(const zest::Result<LhsT, LhsE>& lhs, const zest::Result<RhsT, RhsE>& rhs) {
-    return lhs.value == rhs.value;
-}
-
-/**
- * @brief Compare a Result instance with an instance of its error type. If the Result instance
- * contains the same error value, evaluates to true. If the Result instance does not contain the
- * same error value, evaluates to false.
- *
- * @tparam T "normal" type of the Result instance
- * @tparam E "error" type of the Result instance
- *
- * @param result Result instance to compare
- * @param error error instance to compare
- *
- * @return true instance contains the same error value.
- * @return false instance does not contain the same error value.
- */
-template<typename T, typename E>
-constexpr bool operator==(const zest::Result<T, E>& result, E error) {
-    if (!result.error.has_value())
-        return false;
-    return result.error.value() == error;
-}
